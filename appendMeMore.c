ffb5ee4bc4503d8b76befa4c4b63e1af0ba28055
huseyin-yildiz <huseyin3421___@hotmail.com>
2023-03-24 10:42:03 +0300

    first part

---
+++ appendMeMore.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#incl
+
+
+int main(int argc, char const *argv[])
+{
+
+    int flags = 
+
+    if(argc == 2)
+    {
+
+    }
+    
+    else if(argc == 3)
+    {
+
+    }
+    
+    else
+        printf("Wrong Parameters ! Usage: appendMeMore filename num-bytes [x] (x is optional) ");
+    
+    
+    
+    
+    
+
+
+    
+    
+    return 0;
+}
